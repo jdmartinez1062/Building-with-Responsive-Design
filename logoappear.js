eaa0e58d296663dcc42c10de216b0f7b8248c710
Juan David Martínez Cubillos <jd.martinez1062@gmail.com>
2020-09-24 17:19:15 -0500

    updated response to be according to the actual page

--- logoappear.js
+++ logoappear.js
@@ -2,17 +2,14 @@
 var myID = document.getElementById('logo');
 
 
-var myScrollFunc = function()
-{
+var myScrollFunc = function () {
     var y = window.scrollY;
-    if (y >= 32) 
-        {
-            myID.className = 'nav-logo show'
-        } 
-    else 
-        {
-            myID.className = 'nav-logo hide'
-        }
+    if (y >= 0) {
+        myID.className = 'nav-logo show'
+    }
+    else {
+        myID.className = 'nav-logo hide'
+    }
 };
 
 window.addEventListener("scroll", myScrollFunc);
